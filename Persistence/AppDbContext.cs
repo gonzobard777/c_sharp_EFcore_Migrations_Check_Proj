983a795455cf2ca5eea2bfe6d7780f50bc1033c0
gonzobard777 <gonzo.bard@dwfe.ru>
2023-06-18 10:07:03 +0300

    init

---
+++ Persistence/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Persistence.Entities;
+
+namespace Persistence;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<Book> Books { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder
+            .UseNpgsql("host=127.0.0.1;port=6172;database=db;username=root;password=12345");
+    }
+}
---
+++ Persistence/Entities/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace Persistence.Entities;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+
+    public int AuthorId { get; set; }
+    public Author Author { get; set; }
+    
+    // public ICollection<Author> Authors { get; set; }
+}
